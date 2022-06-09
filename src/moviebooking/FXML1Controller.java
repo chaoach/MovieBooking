7bd8b6cfe1bd79d78400b2bb3df5d387194ac994
chaoach <alexandrenguyen00@gmail.com>
2022-06-09 16:57:30 +0100

    2

--- src/moviebooking/FXML1Controller.java
+++ src/moviebooking/FXML1Controller.java
@@ -5,11 +5,18 @@
  */
 package moviebooking;
 
-import java.awt.Button;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
 
 /**
  * 1 ERE PAGE QUI DEMANDE SI ON EST GUEST, MEMBER OU EMPLOYEE
@@ -35,9 +42,24 @@ public class FXML1Controller implements Initializable {
     
     @FXML
     private Button employeeButton;
+    
+   
 
     @FXML
     private Button guestButton;
+    
+    @FXML
+    void handleGuestAction(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("FXML2Guest.fxml"));
+        //FXMLDocumentController s2Controller = loader.getController();
+        Parent root1 =(Parent) loader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root1));
+        stage.setTitle("Second Window");
+        stage.show();
+    }
+    
+    //
 
     @FXML
     private Button memberButton;
--- src/moviebooking/MovieBooking.java
+++ src/moviebooking/MovieBooking.java
@@ -5,17 +5,55 @@
  */
 package moviebooking;
 
+//import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  *
  * @author PC
  */
-public class MovieBooking {
-//// OYYOYOYO
+public class MovieBooking extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXML1.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
     }
     
 }
+
+        
+        
+    /*    extends Application{
+
+    public static void main(String[] args) {
+       
+        launch(args);
+    }
+    
+    public void launch(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("FXML1.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+}
+*/
