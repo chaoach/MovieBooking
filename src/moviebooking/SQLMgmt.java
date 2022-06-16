67ee42585451d8b41711f958e5fbb98011df2e4c
rakul_pro <39516108+rakulpro@users.noreply.github.com>
2022-06-16 16:44:15 +0100

    push
    push rakul

--- src/moviebooking/SQLMgmt.java
+++ src/moviebooking/SQLMgmt.java
@@ -66,6 +66,7 @@ public static void main(String[] args) {
                 String user      = "root";
                 String password  = "";
 
+<<<<<<< Updated upstream
                 // create a connection to the database
                 conn = DriverManager.getConnection(url, user, password);
                 // more processing here
@@ -74,6 +75,159 @@ public static void main(String[] args) {
                 ResultSet rs=stmt.executeQuery("select * from movies"); 
                 while(rs.next()) 
                 System.out.println(rs.getInt(1)+"  "+rs.getString(2)); 
+=======
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        return list_movie;
+    }
+    
+    public ArrayList<String> SQLmovie(String date_choisi) {
+
+        ArrayList<String> list_movie = new ArrayList<String>();
+
+        Connection conn = null;
+        try {
+            // db parameters - ptest is the name of the database
+            String url = "jdbc:mysql://localhost:3306/site_cinema";
+            String user = "root";
+            String password = "";
+
+            // create a connection to the database
+            conn = DriverManager.getConnection(url, user, password);
+            // more processing here
+            // ...   
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select movie_name from session where date = '" + date_choisi + "'");
+            while (rs.next()) {
+                list_movie.add(rs.getString(1));//System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3) + "  " + rs.getString(4) + "  " + rs.getString(5) + "  " + rs.getString(6));
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        return list_movie;
+    }
+
+    public ArrayList<String> SQLtime(String date_choisi, String movie_choisi) {
+
+        ArrayList<String> list_time = new ArrayList<String>();
+
+        Connection conn = null;
+        try {
+            // db parameters - ptest is the name of the database
+            String url = "jdbc:mysql://localhost:3306/site_cinema";
+            String user = "root";
+            String password = "";
+
+            // create a connection to the database
+            conn = DriverManager.getConnection(url, user, password);
+            // more processing here
+            // ...   
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select time from session where date = '" + date_choisi + "' and movie_name = '" + movie_choisi + "' ");
+            while (rs.next()) {
+                list_time.add(rs.getString(1));//System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3) + "  " + rs.getString(4) + "  " + rs.getString(5) + "  " + rs.getString(6));
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        return list_time;
+    }
+
+    public ArrayList<String> SQLscreen(String date_choisi, String movie_choisi, String time_choisi) {
+
+        ArrayList<String> list_time = new ArrayList<String>();
+
+        Connection conn = null;
+        try {
+            // db parameters - ptest is the name of the database
+            String url = "jdbc:mysql://localhost:3306/site_cinema";
+            String user = "root";
+            String password = "";
+
+            // create a connection to the database
+            conn = DriverManager.getConnection(url, user, password);
+            // more processing here
+            // ...   
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select screen_n from session where date = '" + date_choisi + "' and movie_name = '" + movie_choisi + "' and time = '" + time_choisi + "' ");
+            while (rs.next()) {
+                list_time.add(rs.getString(1));//System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3) + "  " + rs.getString(4) + "  " + rs.getString(5) + "  " + rs.getString(6));
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        return list_time;
+    }
+
+    public void SQLAddreservation(String date_choisi, String movie_choisi, String time_choisi, String screen, String nom, String prenom, String mail,String nb_place) {
+        Connection conn = null;
+
+        System.out.println(date_choisi);
+        System.out.println(movie_choisi);
+        System.out.println(time_choisi);
+        System.out.println(screen);
+        try {
+            // create a connection to the database
+            conn = DriverManager.getConnection(url, user, password);
+
+            ///////// code of the method
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate("INSERT INTO site_reservation (name_movie, date_movie,time_movie,screen,nom,prenom,mail) VALUES ('" + movie_choisi + "','" + date_choisi + "','" + time_choisi + "','" + screen + "','" + nom + "','" + prenom + "','" + mail + "')");   //SQL command to add movie and price to table movie  
+            System.out.println("movie add");
+
+            ///////// end of code of the method
+        } catch (SQLException e) { //// catch error connection
+            System.out.println(e.getMessage());
+        } finally { //// try catch closing connection
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+    
+}
+>>>>>>> Stashed changes
 
             } catch(SQLException e) {
                  System.out.println(e.getMessage());
